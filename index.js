c32e9f524e64de97dcb799eaa50498ec2dcf7071
Akhil Bharti <akhil.bh96@gmail.com>
2018-06-14 20:40:17 +0530

    mongoDB c cconnectivity error

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const MetaAuth = require('meta-auth');
 var pug = require('pug');
+var MongoClient = require('mongodb').MongoClient;
+var db_url = 'mongodb://localhost/crypto_users';
+
+ 
 const app = express();
 const metaAuth = new MetaAuth();
 const url = require('url');  
@@ -16,10 +20,21 @@ app.get('/send',function(req,res){
     var user_id = req.param('id');
     res.render('donate.pug', { address: user_id });
 });
+app.get('/register',metaAuth,function (req,res){
+    var name = req.param('name');
+    var email = req.param('email');
+    console.log(name,email) //save this in db alog with last entry
+
+});
 app.get('/auth/:MetaMessage/:MetaSignature', metaAuth, (req, res) => {
   if (req.metaAuth && req.metaAuth.recovered) {
     // Signature matches the cache address/challenge
     // Authentication is valid, assign JWT, etc.
+    console.log(req.metaAuth.recovered)
+    MongoClient.connect(db_url, function(err, db) {
+    console.log(db.getName())
+
+});//store this in db
     res.send(req.metaAuth.recovered);
   } else {
     // Sig did not match, invalid authentication
