f602f5d6d4a44b3b31b07ce929ee76a098cca4f4
arvindkalra <kalarvind97@gmail.com>
2018-06-20 00:56:48 +0530

    created seperate js file for all mongo functions

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const MetaAuth = require('meta-auth');
 var pug = require('pug');
@@ -7,14 +8,18 @@ const assert = require('assert');
 const db_url = 'mongodb://localhost:27017';
 const dbName = 'crypto_users';
 
- 
+const mongo = require('./mongo.js');
+
 const app = express();
 const metaAuth = new MetaAuth();
 const url = require('url');  
 
 app.use('/', express.static('.'));
 
-
+app.use(function (req, res, next) {
+   console.log(req.url);
+   next();
+});
 
 app.get('/auth/:MetaAddress', metaAuth, (req, res) => {
   // Request a message from the server
@@ -22,73 +27,54 @@ app.get('/auth/:MetaAddress', metaAuth, (req, res) => {
     res.send(req.metaAuth.challenge)
   }
 });
+
 app.get('/send',function(req,res){
     var user_id = req.param('id');
     res.render('donate.pug', { address: user_id });
 });
+
 app.get('/register',metaAuth,function (req,res){
     var name = req.param('name');
     var email = req.param('email');
-    MongoClient.connect(db_url, function(err, client) {
-        assert.equal(null, err);
-        console.log("REGISTER: Connected correctly to server");
-
-        const db = client.db(dbName);
-        var condn= {_id:-1};
-        var element;
-        db.collection('user_data').find().sort(condn).toArray(function(err, result) {  
-            if (err) throw err;
-
-            //console.log(result[0]);
-            //update record where result[0]
-            element = result[0];
-            db.collection('user_data').updateOne(element, {$set: {name: name,email:email}});
-          });
+    mongo.registerWithNameAndEmail(name, email, function () {
+        console.log(name, email); //save this in db alog with last entry
+        res.render('success.pug')
     });
-    console.log(name,email) //save this in db alog with last entry
-    res.render('success.pug')
 });
+
 app.get('/msg',(req,res) => {
-      var address = req.param('address');
-      var msg = req.param('msg');
-      //console.log(req);
-      console.log(address,msg);
-      MongoClient.connect(db_url, function(err, client) {
-            assert.equal(null, err);
-            console.log("Connected correctly to server");
-
-            const db = client.db(dbName);
-          db.collection('messages').insertOne({address:address,message:msg}, function(err, r) {
-              assert.equal(null, err);
-              assert.equal(1, r.insertedCount);
-                client.close();
-      });
-        });
+     var address = req.param('address');
+     var msg = req.param('msg');
+     //console.log(req);
+     console.log(address,msg);
+     mongo.storeMessageinDb(address, msg, function () {
+         // TODO send message to bot
+         res.send();
+     })
 });
+
 app.get('/auth/:MetaMessage/:MetaSignature', metaAuth, (req, res) => {
-  if (req.metaAuth && req.metaAuth.recovered) {
-    // Signature matches the cache address/challenge
-    // Authentication is valid, assign JWT, etc.
-    console.log(req.metaAuth.recovered);//store this in db
-    var val = req.metaAuth.recovered;
-    MongoClient.connect(db_url, function(err, client) {
-        assert.equal(null, err);
-        console.log("Connected correctly to server");
-
-        const db = client.db(dbName);
-      db.collection('user_data').insertOne({address:val}, function(err, r) {
-          assert.equal(null, err);
-          assert.equal(1, r.insertedCount);
-            client.close();
-      });
+    if (req.metaAuth && req.metaAuth.recovered) {
+        // Signature matches the cache address/challenge
+        // Authentication is valid, assign JWT, etc.
+        console.log(req.metaAuth.recovered);//store this in db
+        var val = req.metaAuth.recovered;
+        mongo.storeAddressinDB(val, function () {
+            res.send(req.metaAuth.recovered);
+        });
+    } else {
+        // Sig did not match, invalid authentication
+        res.status(400).send();
+    };
 });
-    res.send(req.metaAuth.recovered);
-  } else {
-    // Sig did not match, invalid authentication
-    res.status(400).send();
-  };
+
+// This will act as a webhook on which the telegram api will make HTTP Request
+app.post('/message-received', (req, res) => {
+
 });
 
 app.listen(3001, () => {
-  console.log('Listening on port 3001')
-})
+    mongo.connect(function () {
+        console.log('Listening on port 3001');
+    });
+});
