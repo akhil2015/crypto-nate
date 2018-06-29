9d65454767f7957f336a5daa7447fbd1aee47946
arvindkalra <kalarvind97@gmail.com>
2018-06-29 23:35:13 +0530

    created seperate directory for all js files

---
+++ public_static/javascript/notfound.js
@@ -0,0 +1,3 @@
+$('#get').click(function () {
+    alert("Please Install Metamask, Refresh the Page Once Installed. \nUse Link - https://metamask.io/");
+});
--- public_static/notfound.js
+++
@@ -1,3 +0,0 @@
-$('#get').click(function () {
-    alert("Please Install Metamask, Refresh the Page Once Installed");
-});
