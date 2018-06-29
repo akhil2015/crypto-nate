878b5d0160a209e686bb6e27cf486e66575e0ca4
arvindkalra <kalarvind97@gmail.com>
2018-06-29 16:56:27 +0530

    for moving from step 3 to step 2

--- public_static/form-actions.js
+++ public_static/form-actions.js
@@ -122,7 +122,6 @@ $('#next').on('click', function() {
     });
 });
 
-
 $(".submit").click(function() {
     let otp = $('#otp').val();
     let address = $('#address').val();
@@ -148,4 +147,41 @@ $(".submit").click(function() {
             }
         });
     }
+});
+
+$('#prev').click(function () {
+
+    animating = true;
+
+    current_fs = $(this).parent();
+    let previous_fs = $(this).parent().prev();
+
+    //activate next step on progressbar using the index of next_fs
+    $("#progressbar li").eq($("fieldset").index(current_fs)).removeClass("active");
+
+    //show the previous fieldset
+    previous_fs.show();
+    //hide the current fieldset with style
+    current_fs.animate({opacity: 0}, {
+        step: function(now) {
+            //as the opacity of current_fs reduces to 0 - stored in "now"
+            //1. scale previous_fs from 80% to 100%
+            scale = 0.8 + (1 - now) * 0.2;
+            //2. take current_fs to the right(50%) - from 0%
+            left = ((1-now) * 50)+"%";
+            //3. increase opacity of previous_fs to 1 as it moves in
+            opacity = 1 - now;
+            current_fs.css({'left': left});
+            previous_fs.css({'transform': 'scale('+scale+')', 'opacity': opacity});
+        },
+        duration: 1000,
+        complete: function(){
+            current_fs.hide();
+            // previous_fs.attr("style", "display : block; left : 0%, opacity : 1")
+            previous_fs.css({'transform' : '', 'position' : ''});
+            animating = false;
+        },
+        //this comes from the custom easing plugin
+        easing: 'easeInOutBack'
+    });
 });
